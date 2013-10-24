060bf3686a1859aea08a78ec7c50e54e7865808e
John Rogers <johnrogers@eduroam-230-5.ucsc.edu>
2013-10-23 18:10:59 -0700

    added more conditions

---
+++ src/edu/ucsc/gameAI/conditions/GhostInRegion.java
@@ -0,0 +1,32 @@
+package edu.ucsc.gameAI.conditions;
+
+import edu.ucsc.gameAI.ICondition;
+import pacman.game.Game;
+import pacman.game.Constants;
+
+public class GhostInRegion implements ICondition {
+	
+	int boundx1, boundx2, boundy1, boundy2;
+	public GhostInRegion(int x1, int y1, int x2, int y2) {
+		boundx1 = x1;
+		boundx2 = x2;
+		boundy1 = y1;
+		boundy2 = y2;
+	}
+	
+	public boolean test(Game game) {
+		for (GHOST ghost : GHOST.values()) {
+			int[] indices = game.getGhostCurrentNodeIndex(ghost)
+			for (int i=0; i<indices.length; i++) {
+				yCoord = game.getNodeYCood(indices[i]);
+				if (yCoord >= y1 && yCoord <= y2) {
+					xCoord = game.getNodeXCood(indices[i]);
+					if (xCoord >= x1 && xCoord <= x2) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+}
---
+++ src/edu/ucsc/gameAI/conditions/PillInRegion.java
@@ -0,0 +1,29 @@
+package edu.ucsc.gameAI.conditions;
+
+import edu.ucsc.gameAI.ICondition;
+import pacman.game.Game;
+
+public class PillInRegion implements ICondition {
+	
+	int boundx1, boundx2, boundy1, boundy2;
+	public PillInRegion(int x1, int y1, int x2, int y2) {
+		boundx1 = x1;
+		boundx2 = x2;
+		boundy1 = y1;
+		boundy2 = y2;
+	}
+	
+	public boolean test(Game game) {
+		int[] indices = game.getActivePillIndices();
+		for (int i=0; i<indices.length; i++) {
+			yCoord = game.getNodeYCood(indices[i]);
+			if (yCoord >= y1 && yCoord <= y2) {
+				xCoord = game.getNodeXCood(indices[i]);
+				if (xCoord >= x1 && xCoord <= x2) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
